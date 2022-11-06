fc74ca3e1f1d01a2ee551b669daa3e471e9cf17c
hama krimi <hamakrimi@gmail.com>
2022-11-06 14:40:11 +0100

    d

--- src/Controller/IndexControlller.php
+++ src/Controller/IndexControlller.php
@@ -21,60 +21,14 @@ class IndexControlller extends AbstractController
     }
 
     /**
-     * @Route("/offres", name="offres")
+     * @Route("/", name="offres")
      */
     public function index(){
-        $jobs = $this->manager->getRepository(Jobs::class)->findAll();
-      
-        foreach ($jobs as $job){
-             $title=$job->getTitle();
-
-              $skills=[];
-            foreach($job->getSkills() as $skill)
-            {
-             $skills[]=[
-             'id'=>$skill->getId() ,
-             'title'=>$skill->getTitle()
-             ] ;
-
-            }
-            $jobsArray[] = [
-                'id' => $job->getId(),
-                'title' => $job->getTitle(),
-                'country' => $job->getCountry(),
-                'image'=>$job->getImage(),
-                'city' => $job->getCity(),
-                'beginAt' => $job->getCreatedAt(),
-                'expireAt' => $job->getExpiredAt(),
-                'type' => $job->getTypeid()->getTitle(),
-                'exp' => $job->getExp()->getTitle() ,
-                'presentation' => $job->getPresentation(),
-                'resp' => $job->getResp() ,
-                'req' => $job->getReq (),
-                'slug'=> strtolower(str_replace(' ', '-',$title))
-                
-                
-
-
-                
-
-
-            ];
-        }
+       
         
 
-        return $this->render('offres/index.html.twig', [
-            'jobs' => $jobsArray 
-        ]);
+        return $this->render('offres/index.html.twig');
     }
 
-    /**
-     * @Route("/offres/{slug}/{id}", name="offre_get")
-     */
-     public function details(Jobs $job): Response
-    {
-
-     
-        return $this->render('offres/offre.html.twig', compact('job'));
-    }
+  
 }
